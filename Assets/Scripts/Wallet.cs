61b66583ceb3ca92942b537d525ad98dda89ed72
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-11-20 16:49:01 +0300

    start

--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -8,5 +8,13 @@ public class Wallet
 
     private int _maxValue;
 
+    public Wallet(int coinValue, int diamondValue, int energyValue, int maxValue)
+    {
+        _coinValue = coinValue;
+        _diamondValue = diamondValue;
+        _energyValue = energyValue;
+        _maxValue = maxValue;
+    }
+
 
 }
